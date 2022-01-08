88b4523cd8d0f8db8d80c5a8d1218cefd37bc9c1
adityanaranje <aditya.naranje7@gmail.com>
2022-01-08 17:27:15 +0530

    make it better

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 mkdir -p ~/.streamlit/
 
-echo "\
+echo "[theme]
+primaryColor="#13fbe6"
+backgroundColor="#080808"
+secondaryBackgroundColor="#22222d"
+textColor="#fffefe"
+font="serif"
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
